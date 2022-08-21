3f6b85d28bd84b2b2cb87484ae1b360bd6be9bc7
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2022-08-21 16:48:29 +0530

    Updated code into modules

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 const container = document.querySelector('#container');
 const clearBtn = document.querySelector('#clear');
 const changeSizeBtn = document.querySelector('#change-size');
-const divs = document.getElementsByClassName('sketch-divs');
+let squareSize = 16;
+
 
 
 //    DEFAULT GRID CREATION
-createDivs();
+createDivs(16);
 
-function createDivs() {
-    for (let i = 0 ; i < 16 ; i++) {
-        for (let j = 0 ; j < 16 ; j++) {
+function createDivs(squareSize) {
+    for (let i = 0 ; i < squareSize ; i++) {
+        for (let j = 0 ; j < squareSize ; j++) {
     
             const div = document.createElement('div');
             div.classList.add('sketch-divs');
             container.appendChild(div);
         }
     } 
-    container.setAttribute(`style`,`grid-template-columns: repeat(16,1fr); grid-template-rows: repeat(16,1fr);`);
+    container.setAttribute(`style`,`grid-template-columns: repeat(${squareSize},1fr); grid-template-rows: repeat(${squareSize},1fr);`);
 }
 
+
+function removeDivs() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+}
+
+
+
+const divs = document.querySelectorAll('.sketch-divs');
+console.log(divs);
 
 //    EVENT TO DETECT HOVER AND CHANGE COLOR OF DIVS
 
-for (const div of divs) {
-    div.addEventListener('mouseover', function() {
-        console.log('Clicked a div');
-        div.classList.add('add-bg');
+function sketchCanvas() {
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', function() {
+            console.log('Clicked a div');
+            div.style.background = 'black';
+        });
     });
-    
 }
+
+sketchCanvas();
 
 
 //     CLEAR ALL BUTTON
@@ -37,8 +52,9 @@ clearBtn.addEventListener('click', function(){
 
     const divs = document.querySelectorAll('.sketch-divs');
     for (const div of divs) {
-        div.classList.remove('add-bg');
+        div.style.background = 'transparent';
     }
+    sketchCanvas();
 });
 
 
@@ -48,7 +64,7 @@ changeSizeBtn.addEventListener('click', function() {
 
     //    ASKING USER FOR PROMPT
 
-    let squareSize = Number(prompt("Total number of squares? (Maximum: 100)"));
+    let squareSize = Number(prompt("Enter square number for canvas (Max: 100)"));
 
     if (squareSize > 100) {
         alert("Exceeded maximum limit. Try again");
@@ -62,23 +78,15 @@ changeSizeBtn.addEventListener('click', function() {
     
     //    REMOVING ALL DIVS PRESENT BEFORE
 
-    const divs = document.querySelectorAll('.sketch-divs');
-    for (const div of divs) {
-        container.removeChild(div);
-    }
+    removeDivs();
 
 
     //    ADDING NEW DIVS ACC TO SIZE CHOSEN BY USER
     
-    for (let i = 0 ; i < squareSize ; i++) {
-        for (let j = 0 ; j < squareSize ; j++) {
-    
-            const div = document.createElement('div');
-            div.classList.add('sketch-divs');
-            container.appendChild(div);
-        }
-    }
+    createDivs(squareSize);
 
-    container.setAttribute(`style`,`grid-template-columns: repeat(${squareSize},1fr); grid-template-rows: repeat(${squareSize},1fr);`);
+    console.log(divs);
 
 });
+
+sketchCanvas();
